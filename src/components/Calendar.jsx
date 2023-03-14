b95206d7d640429427cb092681e059f2541d3188
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2023-03-14 13:45:56 -0700

    Tailwind Bug
    Progress has slowed down due to issues rendering styles conditionally
    with tailwind css. background color on the
    modal component eont register when event color is present

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Dna } from "react-loader-spinner";
 import { motion } from "framer-motion";
 import Modal from "./Modal";
 
@@ -13,6 +14,7 @@ const weekDays = [
 ];
 
 const Calendar = ({ date }) => {
+  const [loading, setLoading] = useState(true);
   const [events, setEvents] = useState(
     localStorage.getItem("events") ? localStorage.getItem("events") : []
   );
@@ -44,6 +46,7 @@ const Calendar = ({ date }) => {
   }, [date]);
 
   useEffect(() => {
+    //localStorage.removeItem("events");
     setFirstDayOfMonth(new Date(year, month, 1));
     setDaysInMonth(new Date(year, month + 1, 0).getDate());
   }, [year, month]);
@@ -61,6 +64,7 @@ const Calendar = ({ date }) => {
 
   useEffect(() => {
     setPaddingDays(weekDays.indexOf(dateString.split(", ")[0]));
+    setLoading(false);
   }, [dateString]);
 
   const [string, setString] = useState("");
@@ -80,43 +84,65 @@ const Calendar = ({ date }) => {
         ))}
       </div>
       <section>
-        <div className="grid grid-cols-7 gap-1 h-[80vh]">
-          {[...Array(paddingDays + daysInMonth)].map((abs, index) => (
-            <div
-              onClick={() =>
-                index >= paddingDays &&
-                addEvent(`${month + 1}/${index - paddingDays + 1}/${year}`)
-              }
-              key={index}
-              className={`w-full h-full p-2 rounded-sm shadow-sm hover:shadow-blue-300 duration-200 ${
-                index - paddingDays + 1 === day &&
-                month === new Date().getMonth() &&
-                year === new Date().getFullYear() &&
-                "shadow-green-400 shadow-md"
-              }`}
-            >
-              <p className="text-center">
-                {index >= paddingDays && index - paddingDays + 1}
-              </p>
-              {events.length > 0 &&
-                JSON.parse(events).map((event) => (
-                  <motion.div
-                    initial={{ opacity: 0, y: -50 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    key={event.event}
-                    className={`w-full rounded-lg bg-${event.color}-400 overflow-x-hidden mt-1 shadow-sm`}
-                  >
-                    {event.date ===
-                      `${month + 1}/${index - paddingDays + 1}/${year}` && (
-                      <p className="whitespace-nowrap my-1 mx-2 text-xs">
-                        {event.event}
-                      </p>
-                    )}
-                  </motion.div>
-                ))}
-            </div>
-          ))}
-        </div>
+        {!loading ? (
+          <div className="grid grid-cols-7 gap-1 h-[80vh]">
+            {[...Array(paddingDays + daysInMonth)].map((abs, index) => (
+              <div
+                onClick={() =>
+                  index >= paddingDays &&
+                  addEvent(`${month + 1}/${index - paddingDays + 1}/${year}`)
+                }
+                key={index}
+                className={`w-full h-full rounded-sm shadow-sm hover:shadow-blue-300 duration-200 flex flex-col items-center justify-start overflow-hidden ${
+                  index - paddingDays + 1 === day &&
+                  month === new Date().getMonth() &&
+                  year === new Date().getFullYear() &&
+                  "shadow-green-400 shadow-md"
+                }`}
+              >
+                <p
+                  className={`text-center text-sm mb-[2px] ${
+                    index - paddingDays + 1 === day &&
+                    month === new Date().getMonth() &&
+                    year === new Date().getFullYear() &&
+                    ""
+                  }`}
+                >
+                  {index >= paddingDays && index - paddingDays + 1}
+                </p>
+                {events.length > 0 &&
+                  JSON.parse(events).map((event) => (
+                    <>
+                      {event.date ===
+                        `${month + 1}/${index - paddingDays + 1}/${year}` && (
+                        <motion.div
+                          initial={{ opacity: 0, y: -50 }}
+                          animate={{ opacity: 1, y: 0 }}
+                          key={event.event}
+                          className={`w-full rounded-lg bg-${event.color}-400 overflow-x-hidden shadow-md p-1`}
+                        >
+                          <p className="whitespace-nowrap text-xs">
+                            {event.event}
+                          </p>
+                        </motion.div>
+                      )}
+                    </>
+                  ))}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="w-full h-full flex justify-center items-center">
+            <Dna
+              visible={loading}
+              height="80"
+              width="80"
+              ariaLabel="dna-loading"
+              wrapperStyle={{}}
+              wrapperClass="dna-wrapper"
+            />
+          </div>
+        )}
       </section>
       {openModal && (
         <Modal
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
 const Modal = ({ selectedDate, setModal, events, setEvents }) => {
+  const [modalColor, setModalColor] = useState(false);
   const [eventText, setEventText] = useState("");
+  const [dayEvents, setDayEvents] = useState([]);
   const [color, setColor] = useState("blue");
+  const [repeat, setRepeat] = useState(false);
+  const [time, setTime] = useState(new Date(selectedDate).setHours(8));
+  const [reminder, setReminder] = useState(false);
+  const [addNewEvent, setAddNewEvent] = useState(false);
+
+  useEffect(() => {
+    const arrayOfEvents = JSON.parse(events);
+    const eventsForDay = arrayOfEvents.filter(
+      (event) => event.date === selectedDate
+    );
+    eventsForDay[0]
+      ? setModalColor(eventsForDay[0].color)
+      : setModalColor(false);
+    setDayEvents(eventsForDay);
+  }, []);
 
   const addEvent = () => {
     const newEvent = {
       event: eventText,
       date: selectedDate,
       color,
+      repeat,
+      time,
+      reminder,
     };
     if (events.length > 0) {
       const arrayOfEvents = JSON.parse(events);
@@ -38,60 +58,90 @@ const Modal = ({ selectedDate, setModal, events, setEvents }) => {
       <motion.div
         initial={{ x: 1000 }}
         animate={{ x: 0 }}
-        className="bg-white rounded-md shadow-md p-2 fixed top-0 bottom-0 right-0 h-full"
+        className={`${
+          modalColor ? `bg-${modalColor}` : "bg-white"
+        } bg-white rounded-md shadow-md p-2 fixed top-0 bottom-0 right-0 h-full flex flex-col items-center justify-center w-[65%]`}
       >
-        <h2 className="bold text-xl">{selectedDate}</h2>
-        <div className="flex flex-wrap justify-center items-center my-20 mx-auto w-[80%]">
-          <div
-            onClick={() => setColor("blue")}
-            className={`bg-blue-400 w-9 h-9 rounded-full shadow-sm m-2 ${
-              color === "blue" && "shadow-lg"
-            }`}
-          ></div>
-          <div
-            onClick={() => setColor("red")}
-            className={`bg-red-400 w-9 h-9 rounded-full shadow-sm m-2 ${
-              color === "red" && "shadow-lg"
-            }`}
-          ></div>
-          <div
-            onClick={() => setColor("green")}
-            className={`bg-green-400 w-9 h-9 rounded-full shadow-sm m-2 ${
-              color === "green" && "shadow-lg"
-            }`}
-          ></div>
-          <div
-            onClick={() => setColor("pink")}
-            className={`bg-pink-400 w-9 h-9 rounded-full shadow-sm m-2 ${
-              color === "pink" && "shadow-lg"
-            }`}
-          ></div>
-          <div
-            onClick={() => setColor("orange")}
-            className={`bg-orange-400 w-9 h-9 rounded-full shadow-sm m-2 ${
-              color === "orange" && "shadow-lg"
-            }`}
-          ></div>
-        </div>
-        <input
-          placeholder="Event"
-          onChange={(e) => setEventText(e.target.value)}
-          className={`p-2 rounded-md m-2 mt-10 text-center shadow-${color}-400 shadow-md outline-none`}
-        />
-        <div className="flex justify-around p-2 mt-10">
-          <button
-            onClick={() => setModal(false)}
-            className="px-5 py-2 rounded-md shadow-md bg-gradient-to-r from-red-400 to-red-300 w-[100px]"
-          >
-            Cancel
-          </button>
-          <button
-            onClick={() => addEvent()}
-            className="px-5 py-2 rounded-md shadow-md bg-gradient-to-r from-green-400 to-green-300 w-[100px]"
-          >
-            Save
-          </button>
-        </div>
+        {addNewEvent ? (
+          <>
+            <h2 className="bold text-xl">{selectedDate}</h2>
+            <div className="flex flex-wrap justify-center items-center my-20 mx-auto w-[80%]">
+              <div
+                onClick={() => setColor("blue")}
+                className={`bg-blue-400 w-9 h-9 rounded-full shadow-sm m-2 ${
+                  color === "blue" && "shadow-lg"
+                }`}
+              ></div>
+              <div
+                onClick={() => setColor("red")}
+                className={`bg-red-400 w-9 h-9 rounded-full shadow-sm m-2 ${
+                  color === "red" && "shadow-lg"
+                }`}
+              ></div>
+              <div
+                onClick={() => setColor("green")}
+                className={`bg-green-400 w-9 h-9 rounded-full shadow-sm m-2 ${
+                  color === "green" && "shadow-lg"
+                }`}
+              ></div>
+              <div
+                onClick={() => setColor("pink")}
+                className={`bg-pink-400 w-9 h-9 rounded-full shadow-sm m-2 ${
+                  color === "pink" && "shadow-lg"
+                }`}
+              ></div>
+              <div
+                onClick={() => setColor("orange")}
+                className={`bg-orange-400 w-9 h-9 rounded-full shadow-sm m-2 ${
+                  color === "orange" && "shadow-lg"
+                }`}
+              ></div>
+            </div>
+            <input
+              placeholder="New Event"
+              onChange={(e) => setEventText(e.target.value)}
+              className={`p-2 rounded-md m-2 mt-10 text-center shadow-${color}-400 shadow-md outline-none`}
+            />
+            <div className="flex justify-around p-2 mt-10 w-full">
+              <button
+                onClick={() => setModal(false)}
+                className="px-5 py-2 rounded-md shadow-md bg-gradient-to-r from-red-400 to-red-300 w-[100px]"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={() => addEvent()}
+                className="px-5 py-2 rounded-md shadow-md bg-gradient-to-r from-green-400 to-green-300 w-[100px]"
+              >
+                Save
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <h2>{selectedDate}</h2>
+            <div className="w-full h-full flex flex-col justify-center items-center">
+              {dayEvents.length > 0 ? (
+                dayEvents.map((event) => (
+                  <div
+                    key={event?.event}
+                    className={`px-2 py-1 m-2 rounded-md bg-${event.color}-400`}
+                  >
+                    <p>{event?.event}</p>
+                  </div>
+                ))
+              ) : (
+                <h2>No Events To Show For Today</h2>
+              )}
+            </div>
+            <button
+              onClick={() => setAddNewEvent(true)}
+              className="px-5 py-2 rounded-md shadow-md bg-gradient-to-r from-red-400 to-red-300"
+            >
+              Add
+            </button>
+          </>
+        )}
       </motion.div>
     </>
   );
