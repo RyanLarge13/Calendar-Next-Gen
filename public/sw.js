9f1eff46866970a0efe1219d1d9f5ccf4e28f3ef
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2023-07-15 10:21:14 -0700

    Notification Upgrades
    Desire to put notification badges,vibrations
    and actions lead to an update in the Service Worker file.

--- public/sw.js
+++ public/sw.js
@@ -24,6 +24,34 @@ self.addEventListener("push", (event) => {
     self.registration.showNotification(title, {
       body,
       icon: "./favicon.svg",
+      badge: "./favicon.svg",
+      vibrate: [200, 100, 200],
+      actions: [{ action: "mark-as-read", title: "Mark as Read" }],
     })
   );
+});
+
+self.addEventListener("notificationclick", (event) => {
+  if (event.action === "mark-as-read") {
+  	return console.log("trying something new!")
+    fetch("https://yourapi.com/mark-as-read", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        notificationId: event.notification.data.notificationId,
+      }),
+    })
+      .then((response) => {
+        event.notification.close();
+        // Handle the API response as needed
+      })
+      .catch((error) => {
+        console.log(`Error marking notification as read: ${error}`);
+      });
+  } else {
+    event.notification.close();
+    event.waitUntil(clients.openWindow("https://yourapp.com"));
+  }
 });
