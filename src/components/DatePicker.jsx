49f3c935f84ea7a2a7393b29f4f527f11d68e629
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2023-09-23 00:19:44 -0400

    DatePicker && Sticky UI
    * Choose month & year to navigate calendar
    * month and year ui picker now snaps to closest element
    + sticky header added

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -2,44 +2,69 @@ import { useContext, useRef, useState, useEffect } from "react";
 import { staticMonths, staticYears } from "../constants.js";
 import { motion } from "framer-motion";
 import InteractiveContext from "../context/InteractiveContext";
+import DatesContext from "../context/DatesContext";
 
 const DatePicker = () => {
   const { setShowDatePicker } = useContext(InteractiveContext);
+  const { setUpdatedDate, setNav, nav } =
+    useContext(DatesContext);
 
-  const scrollContainerRef = useRef(null);
+  const [newYear, setNewYear] = useState(null);
+  const [newMonth, setNewMonth] = useState(null);
 
-  const [closestElement, setClosestElement] = useState(null);
+  const scrollMonthRef = useRef(null);
+  const scrollYearRef = useRef(null);
+  let debounceMonthTimeout;
+  let debounceYearTimeout;
 
-  useEffect(() => {
-    const scrollContainer = scrollContainerRef.current;
-    const handleScroll = () => {
-      const scrollY = scrollContainer.scrollTop;
-      const elements = Array.from(
-        scrollContainer.querySelectorAll(".snap-element")
+  const handleMonthScroll = () => {
+    clearTimeout(debounceMonthTimeout);
+    const scrollPosition = scrollMonthRef.current.scrollTop;
+    const itemHeight = 60;
+    debounceMonthTimeout = setTimeout(() => {
+      const closestIndex = Math.round(scrollPosition / itemHeight);
+      const lastIndex = staticMonths.length - 1;
+      const clampedIndex = Math.round(
+        Math.min(Math.max(closestIndex, 0), lastIndex)
       );
-      let closest = null;
-      let minDistance = Infinity;
-      elements.forEach((element) => {
-        const rect = element.getBoundingClientRect();
-        const distance = Math.abs(rect.top);
-
-        if (distance < minDistance) {
-          minDistance = distance;
-          closest = element;
-        }
+      const targetScrollPosition = clampedIndex * itemHeight;
+      scrollMonthRef.current.scrollTo({
+        top: targetScrollPosition,
+        behavior: "smooth",
       });
-      setClosestElement(closest);
-    };
-    scrollContainer.addEventListener("scroll", handleScroll);
-    return () => {
-      scrollContainer.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+      setNewMonth(clampedIndex);
+    }, 50);
+  };
 
-  const snapToClosestElement = () => {
-    if (closestElement) {
-      closestElement.scrollIntoView({ behavior: "smooth" });
+  const handleYearScroll = () => {
+    clearTimeout(debounceYearTimeout);
+    const scrollPosition = scrollYearRef.current.scrollTop;
+    const itemHeight = 60;
+    debounceYearTimeout = setTimeout(() => {
+      const closestIndex = Math.round(scrollPosition / itemHeight);
+      const lastIndex = staticYears.length - 1;
+      const clampedIndex = Math.round(
+        Math.min(Math.max(closestIndex, 0), lastIndex)
+      );
+      const targetScrollPosition = clampedIndex * itemHeight;
+      scrollYearRef.current.scrollTo({
+        top: targetScrollPosition,
+        behavior: "smooth",
+      });
+      const yearToSet = Number(staticYears[clampedIndex]);
+      setNewYear(yearToSet);
+    }, 50);
+  };
+
+  const submitMonthAndTime = () => {
+    const newDt = new Date();
+    newDt.setMonth(newMonth);
+    newDt.setFullYear(newYear);
+    setUpdatedDate(newDt);
+    if (nav !== 0) {
+      setNav(0);
     }
+    setShowDatePicker(false);
   };
 
   return (
@@ -48,39 +73,48 @@ const DatePicker = () => {
         className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm"
         onClick={() => setShowDatePicker(false)}
       ></div>
-      <div className="fixed top-40 left-[50%]  translate-x-[-50%] rounded-md shadow-md bg-white p-5">
-        <div className="flex">
-          <div className="">
+      <div className="fixed top-40 left-[50%] translate-x-[-50%] rounded-md shadow-md bg-white p-5">
+        <div className="flex gap-x-3">
+          <div>
             <h2 className="text-lg">Month</h2>
             <div
               className="overflow-y-scroll max-h-[60px] text-[40px] scrollbar-hide"
-              ref={scrollContainerRef}
+              ref={scrollMonthRef}
+              onScroll={handleMonthScroll}
             >
-              {staticMonths.map((mon, index) => (
-                <motion.div
-                  key={index}
-                  initial={{ scale: 0 }}
-                  whileInView={{ scale: 1 }}
-                  className="pb-4 snap-element"
-                >
-                  {mon.name}
-                </motion.div>
-              ))}
+              <div>
+                {staticMonths.map((mon, index) => (
+                  <motion.div
+                    key={index}
+                    initial={{ scale: 0 }}
+                    whileInView={{ scale: 1 }}
+                    className="h-[60px]"
+                  >
+                    {mon.name}
+                  </motion.div>
+                ))}
+              </div>
             </div>
           </div>
           <div>
             <h2 className="text-lg">Year</h2>
-            <div className="overflow-y-scroll max-h-[60px] text-[40px] scrollbar-hide">
-              {staticYears.map((stYr, index) => (
-                <motion.div
-                  key={index}
-                  initial={{ scale: 0 }}
-                  whileInView={{ scale: 1 }}
-                  className="pb-4"
-                >
-                  {stYr}
-                </motion.div>
-              ))}
+            <div
+              className="overflow-y-scroll max-h-[60px] text-[40px] scrollbar-hide"
+              ref={scrollYearRef}
+              onScroll={handleYearScroll}
+            >
+              <div>
+                {staticYears.map((stYr, index) => (
+                  <motion.div
+                    key={index}
+                    initial={{ scale: 0 }}
+                    whileInView={{ scale: 1 }}
+                    className="h-[60px]"
+                  >
+                    {stYr}
+                  </motion.div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
@@ -88,7 +122,9 @@ const DatePicker = () => {
           <button type="text" onClick={() => setShowDatePicker(false)}>
             Cancel
           </button>
-          <button type="text">Okay</button>
+          <button type="text" onClick={() => submitMonthAndTime()}>
+            Okay
+          </button>
         </div>
       </div>
     </>
--- src/components/Stickies.jsx
+++ src/components/Stickies.jsx
@@ -1,28 +1,49 @@
 import { useContext } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import {
+  AiFillPushpin,
+  AiFillCloseCircle,
+  AiOutlinePushpin,
+} from "react-icons/ai";
+import { BiExpand } from "react-icons/bi";
 import UserContext from "../context/UserContext";
 
 const Stickies = () => {
   const { stickies, setStickies } = useContext(UserContext);
 
   return (
-    <>
+    <AnimatePresence>
       {stickies.map((sticky) => (
         <motion.div
+          initial={{ scale: 0 }}
+          exit={{ scale: 0 }}
+          animate={{ scale: 1 }}
           drag={true}
           dragConstraints={{
             top: 0,
-            right: window.innerWidth - 100,
+            right: window.innerWidth - 150,
             left: 0,
-            bottom: window.innerHeight - 100,
+            bottom: window.innerHeight - 150,
           }}
-          className={`markdown z-[999] shadow-xl rounded-md fixed top-0 left-0 w-40 h-40 px-2 py-1 overflow-y-auto ${sticky.color}`}
+          className={`markdown z-[999] shadow-xl rounded-md fixed top-0 left-0 w-40 h-40 overflow-y-auto ${sticky.color}`}
         >
-          <h2>{sticky.title}</h2>
-          <div dangerouslySetInnerHTML={{ __html: sticky.body }}></div>
+          <div className="rounded-t-md bg-slate-100 shadow-md p-2 flex justify-between items-center">
+            <div className="flex justify-between items-center gap-x-3">
+              <AiOutlinePushpin />
+              <BiExpand />
+            </div>
+            <div>
+              <AiFillCloseCircle />
+            </div>
+          </div>
+
+          <div className="p-2">
+            <h2>{sticky.title}</h2>
+            <div dangerouslySetInnerHTML={{ __html: sticky.body }}></div>
+          </div>
         </motion.div>
       ))}
-    </>
+      </AnimatePresence>
   );
 };
 
