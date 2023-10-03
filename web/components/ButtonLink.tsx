4c9c60bb8bef597058ec275ca25066639ee20fa4
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2023-10-03 00:25:13 -0400

    TypeScript Typo
    Adding component proptypes

--- web/components/ButtonLink.tsx
+++ web/components/ButtonLink.tsx
@@ -1,7 +1,22 @@
-import Link from "next/link"
+import Link from "next/link";
 
-const ButtonLink = ({styles, route, text, type}) => {
-	return <button type={type} className={styles}><Link href={route}></Link>{text}</button>
-}
+const ButtonLink = ({
+  styles,
+  route,
+  text,
+  type,
+}: {
+  styles: string;
+  route: string;
+  text: string;
+  type: string;
+}) => {
+  return (
+    <button type={type} className={styles}>
+      <Link href={route}></Link>
+      {text}
+    </button>
+  );
+};
 
-export default ButtonLink
+export default ButtonLink;
