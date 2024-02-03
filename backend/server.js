6f9383b24ebfbc03a264f214cbb703ad588b8d7a
RyanLarge13 <ryanlarge13@gmail.com>
2024-02-03 09:00:34 -0500

    Errrrrr

--- backend/server.js
+++ backend/server.js
@@ -28,10 +28,7 @@ const corsOptions = {
 const app = express();
 app.use(cors(corsOptions));
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://calng.app, https://www.calng.app, https://calendar-next-gen.vercel.app, http://localhost:5173"
-  );
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Credentials", "true");
