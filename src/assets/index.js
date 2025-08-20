07494b6a459513ac05f63f2810034487897df840
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2025-08-20 15:01:23 -0400

    Update Export File

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,19 +1,5 @@
-import Sunny from "./weather-icons-master/svg/wi-day-sunny.svg";
-import Cloudy from "./weather-icons-master/svg/wi-cloudy.svg";
-import Stormy from "./weather-icons-master/svg/wi-thunderstorm.svg";
-import Rainy from "./weather-icons-master/svg/wi-rain.svg";
-import Foggy from "./weather-icons-master/svg/wi-fog.svg";
-import Snowy from "./weather-icons-master/svg/wi-snow.svg";
-import Windy from "./weather-icons-master/svg/wi-windy.svg";
 import Logo from "./favicon.svg";
 
 export {
-  Sunny,
-  Cloudy,
-  Stormy,
-  Rainy,
-  Snowy,
-  Foggy,
-  Windy, 
   Logo,
 };
